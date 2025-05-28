39bb5f1c66c293ffbcd644613a8fa77a4997f4fa
qasimnauman <m.qasimnauman@gmail.com>
2025-05-28 13:41:29 +0500

    fix: streamline dotenv configuration and enhance dbconnect logging

--- backend/src/db/dbconnect.js
+++ backend/src/db/dbconnect.js
@@ -3,6 +3,9 @@ import { DB_NAME } from "../constants.js"
 
 const connectDB = async () => {
     try {
+        // console.log("MONGO_DB_URL:", process.env.MONGO_DB_URL);
+        // console.log("DB_NAME:", DB_NAME);
+        // console.log("Connecting to:", `${process.env.MONGO_DB_URL}/${DB_NAME}`);
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`)
         console.log(`\n Mongo DB Connected !! DB Host: ${connectionInstance.connection.host}`)
     } catch (err) {
