64471b74f2c80a47ee2111f7076abb632aaaf406
qasimnauman <m.qasimnauman@gmail.com>
2025-05-28 13:02:56 +0500

    fix: correct import path casing for asyncHandler utility

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/AsyncHandler.js"
 import { Apierror } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js"
 import { Apiresponse } from "../utils/Apiresponse.js"
